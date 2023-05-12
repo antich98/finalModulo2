7660de50f7caf33d04a3daf7301a6cd83158e838
Ezequiel Monachesi <ezequiel.monachesi@gmail.com>
2023-05-12 20:10:45 -0300

    se corrigieron console logs

--- js/index.js
+++ js/index.js
@@ -2,17 +2,16 @@ let formBuscador = document.querySelector('form');
 let inputBuscador = document.getElementById('inputBuscador');
 let crearCard = document.getElementById('crearCard')
 
-formBuscador.addEventListener('submit', buscarPelicula);
+formBuscador.addEventListener('keyup', buscarPelicula);
 
 function buscarPelicula(e){
     e.preventDefault();
     let aside = document.querySelectorAll('aside')
-    console.log(aside)
-
-    let tarjetas = aside.forEach((asideJuego, indice) => {
+    aside.forEach((asideJuego, indice) => {
         if(asideJuego.textContent.toLocaleLowerCase().includes(inputBuscador.value)){
             aside[indice].classList.remove('displayNone');
         }else{
             aside[indice].classList.add('displayNone');
         }
     });
+}
