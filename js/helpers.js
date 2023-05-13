ae34c8d7e5d0a8742cce9b486060446e92e8cb4f
Ezequiel Monachesi <ezequiel.monachesi@gmail.com>
2023-05-12 23:58:50 -0300

    empezando a crear lógica de helpers

---
+++ js/helpers.js
@@ -0,0 +1,19 @@
+function validarCantidadCaracteres(texto, min, max){
+    if(texto.length >= min && texto.length <= max){
+        console.log('aqui el texto tiene la cant. de caracteres correcto');
+        return true;
+    }else{
+        console.log('aqui el texto no cumple la validacion');
+        return false;
+    }
+}
+
+export function sumarioValidaciones(titulo){
+    let resumen = '';
+    //quiero preguntar si no cumple con la validacion
+    if(!validarCantidadCaracteres(titulo,2,100)){
+        resumen = 'El titulo debe tener entre 2 y 100 caracteres <br>'
+    }
+    
+    return resumen;
+}
