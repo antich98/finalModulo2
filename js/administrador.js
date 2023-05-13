0ca99238fe7caf7e42c3664af0e00a6569d48066
Santiago Antich Llarrull <santiantich98@gmail.com>
2023-05-13 15:18:52 -0300

    Traje los inputs del formulario

--- js/administrador.js
+++ js/administrador.js
@@ -2,6 +2,17 @@ import VideoJuego from "./classVideoJuego.js";
 
 // Variables globales
 let formJuego = document.getElementById("formJuego");
+let codigo = document.getElementById("codigo"),
+nombre = document.getElementById("nombre"),
+descripcion = document.getElementById("descripcion"),
+imagen = document.getElementById("imagen"),
+categoria = document.getElementById("categoria"),
+precio = document.getElementById("precio"),
+requisitos = document.getElementById("requisitos"),
+desarrollador = document.getElementById("desarrollador"),
+anio = document.getElementById("anio"),
+resenias = document.getElementById("resenias");
+
 let listaJuegos = localStorage.getItem("listaJuegos");
 
 // Si listaJuegos está vacía
