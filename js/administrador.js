7a89d52c90396d514cefb86991d2d9aa30c9d832
Santiago Antich Llarrull <santiantich98@gmail.com>
2023-05-13 14:36:33 -0300

    Guardé el juego en un array, y luego ese array en el local storage

--- js/administrador.js
+++ js/administrador.js
@@ -1,4 +1,26 @@
 import VideoJuego from "./classVideoJuego.js";
 
-let nuevoJuego = new VideoJuego(2, "GTA", "1.99", "Rockstar", "Accion", "1", "2", "3", "4", "5");
-console.log(nuevoJuego);
+// Variables globales
+let formJuego = document.getElementById("formJuego");
+let listaJuegos = [];
+
+// Manejadores de eventos
+formJuego.addEventListener("submit", prepararFormulario);
+
+function prepararFormulario(e){
+  e.preventDefault();
+  crearJuego();
+}
+
+function crearJuego(){
+// Validar el formulario
+  let nuevoJuego = new VideoJuego("GTA", "1.99", "Rockstar", "Accion", "1", "2", "3", "4", "5");
+
+  console.log(nuevoJuego);
+// Guardar juego en el array
+  listaJuegos.push(nuevoJuego);
+  console.log(listaJuegos);
+// Guardar el array en el local storage
+  localStorage.setItem("listaJuegos", JSON.stringify(listaJuegos));
+// Limpiar el formulario
+}
