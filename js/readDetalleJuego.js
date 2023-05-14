5d81934867673e07d72d769a7f2b928b7fed4343
danielPomo <danielnicolaspomo@gmail.com>
2023-05-14 05:11:20 -0300

    capturo el codigo unico del juego que quiero mostrar en la ruta dinamica

--- js/readDetalleJuego.js
+++ js/readDetalleJuego.js
@@ -1,2 +1,10 @@
-const parametroCodigo = new URLSearchParams(window.location.search)
-const codigoJuego = parametroCodigo.get("codigo")
+const parametroCodigo = new URLSearchParams(window.location.search);
+const codigoJuego = parametroCodigo.get("codigo");
+
+const listaJuegos = JSON.parse(localStorage.getItem(`listaJuegos`)) || [];
+
+const juego = listaJuegos.find((juego) => juego.codigo === codigoJuego);
+
+function cargarDetalleJuego(juego) {
+    const contenedorDetalle = 
+}
