fc925a3f4ad8ecbc562a105cd8530b637b349ebc
Ezequiel Monachesi <ezequiel.monachesi@gmail.com>
2023-05-14 20:52:31 -0300

    mensaje de juego no encontrado agregado

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 let formBuscador = document.querySelector('form');
 let inputBuscador = document.getElementById('inputBuscador');
-let crearCard = document.getElementById('crearCard')
+let crearCard = document.getElementById('crearCard');
+let noEncontrado = document.createElement('p');
 
 formBuscador.addEventListener('keyup', buscarPelicula);
 
@@ -10,8 +11,15 @@ function buscarPelicula(e){
     aside.forEach((asideJuego, indice) => {
         if(asideJuego.textContent.toLocaleLowerCase().includes(inputBuscador.value)){
             aside[indice].classList.remove('displayNone');
+            noEncontrado.innerHTML = '';
         }else{
             aside[indice].classList.add('displayNone');
+            noEncontrado.innerHTML = '';
+        }
+
+        if(crearCard.children[indice].classList[2] === 'displayNone'){
+            noEncontrado.innerHTML += 'no hya juego'
+            crearCard.appendChild(noEncontrado)
         }
     });
 }
