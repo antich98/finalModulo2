0d986a72dba54974bc376cf8dddfcf0e6c8efb1b
Ezequiel Monachesi <ezequiel.monachesi@gmail.com>
2023-05-14 21:42:25 -0300

    se corrige buscador

--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@ let inputBuscador = document.getElementById('inputBuscador');
 let crearCard = document.getElementById('crearCard');
 let noEncontrado = document.createElement('p');
 
+formBuscador.addEventListener('submit', buscarPelicula);
 formBuscador.addEventListener('keyup', buscarPelicula);
 
 function buscarPelicula(e){
@@ -10,16 +11,19 @@ function buscarPelicula(e){
     let aside = document.querySelectorAll('aside')
     aside.forEach((asideJuego, indice) => {
         if(asideJuego.textContent.toLocaleLowerCase().includes(inputBuscador.value)){
-            aside[indice].classList.remove('displayNone');
             noEncontrado.innerHTML = '';
+            aside[indice].classList.remove('displayNone');
+        }else if(crearCard.children[indice].classList[2] === 'displayNone'){
+            noEncontrado.innerHTML = `
+            <aside class="text-center juegoNoEncontrado">
+                <h3>Juego no encontrado</h3>
+                <p><span class="border border-1 border-secondary-subtle px-2">${inputBuscador.value}<i class="bi bi-exclamation-triangle-fill text-secondary"></i></span></p>
+            </aside>
+            `
+            crearCard.appendChild(noEncontrado)
         }else{
             aside[indice].classList.add('displayNone');
-            noEncontrado.innerHTML = '';
         }
-
-        if(crearCard.children[indice].classList[2] === 'displayNone'){
-            noEncontrado.innerHTML += 'no hya juego'
-            crearCard.appendChild(noEncontrado)
-        }
+        
     });
 }
