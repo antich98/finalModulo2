b35f5171d43821fd8299b2c2073c863ade2d0f62
RLauraS <R_LauraS@yahoo.com.ar>
2023-05-14 21:38:29 -0300

    Una vez que el usuario inicia sesion en el navbar se cambia la opcion a cerrar sesion

--- js/helperIniciarSesion.js
+++ js/helperIniciarSesion.js
@@ -2,10 +2,13 @@
 //^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$
 //PARA VALIDAR CONTRASEÑA:
 //^(?=.*\d)(?=.*[\u0021-\u002b\u003c-\u0040])(?=.*[A-Z])(?=.*[a-z])\S{8,16}$
+
 // Espera a que el documento HTML se cargue completamente antes de ejecutar el código
 window.onload = function () {
-  // Selecciona el botón de iniciar sesión por su ID
   const botonIniciarSesion = document.getElementById("botonIniciarSesion");
+  const botonCerrarSesion = document.getElementById("botonCerrarSesion");
+  const enlaceIniciarSesion = document.getElementById("enlaceIniciarSesion");
+  const enlaceAdministrador = document.getElementById("enlaceAdministrador");
 
   // Asigna un evento 'click' al botón de iniciar sesión
   botonIniciarSesion.addEventListener("click", function (event) {
@@ -23,15 +26,34 @@ window.onload = function () {
       // Muestra una alerta de bienvenida
       alert("¡Bienvenido!");
 
-      // Muestra el enlace de Administrador en el navbar
-      document.getElementById("enlaceAdministrador").classList.remove("d-none");
+      // Cierra la ventana modal
+      const modalIniciarSesion = document.getElementById("modalIniciarSesion");
+      const modalBootstrap = bootstrap.Modal.getInstance(modalIniciarSesion);
+      modalBootstrap.hide();
 
-      // Redirige al usuario a la página de administrador.html
-      window.location.href = "./pages/administrador.html";
+      // Muestra el enlace de Administrador en el navbar
+      enlaceAdministrador.classList.remove("d-none");
+
+      // Cambia el texto del enlace "Iniciar sesión" a "Cerrar sesión"
+      enlaceIniciarSesion.textContent = "Cerrar sesión";
     } else {
       // Muestra un mensaje de error
       alert("Correo electrónico o contraseña incorrectos");
     }
+  });
+
+  // Asigna un evento 'click' al botón de cerrar sesión
+  botonCerrarSesion.addEventListener("click", function (event) {
+    event.preventDefault(); // Evita que el formulario se envíe automáticamente
+
+    // Oculta el enlace de Administrador en el navbar
+    enlaceAdministrador.classList.add("d-none");
+
+    // Cambia el texto del enlace "Cerrar sesión" a "Iniciar sesión"
+    enlaceIniciarSesion.textContent = "Iniciar sesión";
+
+    // Muestra una alerta de cierre de sesión
+    alert("Sesión cerrada correctamente");
   });
 };
 
