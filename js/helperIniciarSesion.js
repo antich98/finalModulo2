845be725682669fdf26406e870ad9d6a17f7b957
RLauraS <R_LauraS@yahoo.com.ar>
2023-05-14 23:07:16 -0300

    Se guarda la sesion iniciada en sesion storage

--- js/helperIniciarSesion.js
+++ js/helperIniciarSesion.js
@@ -4,11 +4,20 @@
 //^(?=.*\d)(?=.*[\u0021-\u002b\u003c-\u0040])(?=.*[A-Z])(?=.*[a-z])\S{8,16}$
 
 // Espera a que el documento HTML se cargue completamente antes de ejecutar el código
-window.onload = function () {
+// JS
+window.addEventListener("load", function () {
   const botonIniciarSesion = document.getElementById("botonIniciarSesion");
-  const botonCerrarSesion = document.getElementById("botonCerrarSesion");
   const enlaceIniciarSesion = document.getElementById("enlaceIniciarSesion");
-  const enlaceAdministrador = document.getElementById("enlaceAdministrador");
+  const modalIniciarSesion = document.getElementById("modalIniciarSesion");
+
+  // Obtén la información de sesión guardada en el sessionStorage
+  const sesionIniciada = sessionStorage.getItem("sesionIniciada");
+
+  // Si la sesión está iniciada, muestra el enlace de Administrador en el navbar
+  if (sesionIniciada) {
+    document.getElementById("enlaceAdministrador").classList.remove("d-none");
+    enlaceIniciarSesion.textContent = "Cerrar sesión";
+  }
 
   // Asigna un evento 'click' al botón de iniciar sesión
   botonIniciarSesion.addEventListener("click", function (event) {
@@ -26,34 +35,43 @@ window.onload = function () {
       // Muestra una alerta de bienvenida
       alert("¡Bienvenido!");
 
-      // Cierra la ventana modal
-      const modalIniciarSesion = document.getElementById("modalIniciarSesion");
-      const modalBootstrap = bootstrap.Modal.getInstance(modalIniciarSesion);
-      modalBootstrap.hide();
+      // Oculta el modal de iniciar sesión
+      modalIniciarSesion.classList.remove("show");
+      modalIniciarSesion.style.display = "none";
+      document.querySelector(".modal-backdrop").remove();
 
       // Muestra el enlace de Administrador en el navbar
-      enlaceAdministrador.classList.remove("d-none");
+      document.getElementById("enlaceAdministrador").classList.remove("d-none");
 
       // Cambia el texto del enlace "Iniciar sesión" a "Cerrar sesión"
       enlaceIniciarSesion.textContent = "Cerrar sesión";
+
+      // Guarda la información de sesión en el sessionStorage
+      sessionStorage.setItem("sesionIniciada", true);
     } else {
       // Muestra un mensaje de error
       alert("Correo electrónico o contraseña incorrectos");
     }
   });
 
-  // Asigna un evento 'click' al botón de cerrar sesión
-  botonCerrarSesion.addEventListener("click", function (event) {
-    event.preventDefault(); // Evita que el formulario se envíe automáticamente
+  // Asigna un evento 'click' al enlace de cerrar sesión
+  enlaceIniciarSesion.addEventListener("click", function (event) {
+    event.preventDefault(); // Evita que el enlace se comporte como un enlace normal
 
-    // Oculta el enlace de Administrador en el navbar
-    enlaceAdministrador.classList.add("d-none");
+    // Si la sesión está iniciada, elimina la información de sesión del sessionStorage
+    if (sessionStorage.getItem("sesionIniciada")) {
+      sessionStorage.removeItem("sesionIniciada");
 
-    // Cambia el texto del enlace "Cerrar sesión" a "Iniciar sesión"
-    enlaceIniciarSesion.textContent = "Iniciar sesión";
+      // Oculta el enlace de Administrador en el navbar
+      document.getElementById("enlaceAdministrador").classList.add("d-none");
 
-    // Muestra una alerta de cierre de sesión
-    alert("Sesión cerrada correctamente");
+      // Cambia el texto del enlace "Cerrar sesión" a "Iniciar sesión"
+      enlaceIniciarSesion.textContent = "Iniciar sesión";
+
+      // Muestra una alerta informando que la sesión se ha cerrado
+      alert("La sesión se ha cerrado correctamente");
+    }
   });
-};
+});
+
 
